03d6020ff708fae73fc2a6e2c66090ede4a75438
Alexey Demyanchuk <alexeydemyanchuk@gmail.com>
2022-03-23 20:16:02 +0100

    Add Heap skeleton code

---
+++ dspy/heap.py
@@ -0,0 +1,34 @@
+"""Max Heap Implementation for integers"""
+
+
+from typing import List
+
+
+class Heap:
+    def __init__(self) -> None:
+        pass
+
+    def __len__(self) -> int:
+        pass
+
+    def insert(self, val: int) -> None:
+        pass
+
+    def _sift_up(self, idx: int) -> None:
+        pass
+
+    def get_max(self) -> int:
+        pass
+
+    def extract_max(self) -> int:
+        pass
+
+    def _sift_down(self, idx: int) -> None:
+        pass
+
+    def _heapify(self, values: List[int]):
+        pass
+
+
+def heap_sort(arr: List[int]):
+    pass
