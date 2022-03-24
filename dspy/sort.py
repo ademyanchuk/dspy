64c9e73f811ee672845e2311dc9f779c1b21d200
Alexey Demyanchuk <demyanchuk.alexey@mh-hannover.de>
2022-03-24 17:06:26 +0100

    Add half of merge sort

---
+++ dspy/sort.py
@@ -0,0 +1,16 @@
+"""Sort Algorithms Implementations"""
+
+from typing import List
+
+
+def merge_sort(arr: List[int]) -> List[int]:
+    if len(arr) < 2:
+        return arr
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+    return merge(left, right)
+
+
+def merge(left: List[int], right: List[int]) -> List[int]:
+    pass
