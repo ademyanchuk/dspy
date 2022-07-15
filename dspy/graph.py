0c2fa5814c87569f00c551ece16f77af77d5a3b1
Alexey Demyanchuk <alexeydemyanchuk@gmail.com>
2022-07-15 15:10:51 +0200

    Add recursive and iterative DFS on list

---
+++ dspy/graph.py
@@ -0,0 +1,62 @@
+from typing import List, Set
+
+
+def dfs_visit_list(
+    i: int, adj_list: List[List[int]], ans: List[int], visited: Set[int]
+):
+    if i in visited:
+        return
+    visited.add(i)
+    for node in adj_list[i]:
+        dfs_visit_list(node, adj_list, ans, visited)
+    ans.append(i)
+
+
+def dfs_list(adj_list: List[List[int]]) -> List[int]:
+    visited = set()
+    ans = []
+    for i in range(len(adj_list)):
+        dfs_visit_list(i, adj_list, ans, visited)
+    return ans
+
+
+def dfs_visit_list_iter(
+    i: int, adj_list: List[List[int]], visited: Set[int]
+) -> List[int]:
+    stack = [i]
+    ans = []
+    while stack:
+        node = stack.pop()
+        if node not in visited:
+            visited.add(node)
+            ans.append(node)
+            for kid in adj_list[node]:
+                if kid not in visited:
+                    stack.append(kid)
+    return list(reversed(ans))
+
+
+def dfs_list_iter(adj_list: List[List[int]]) -> List[int]:
+    visited = set()
+    ans = []
+    for i in range(len(adj_list)):
+        if i not in visited:
+            ans.extend(dfs_visit_list_iter(i, adj_list, visited))
+    return ans
+
+
+if __name__ == "__main__":
+    adj_list = [
+        [5, 3],
+        [3],
+        [2, 0],
+        [0, 1],
+        [],
+        [2],
+    ]
+    ans = [2, 5, 1, 3, 0, 4]
+    got = dfs_list(adj_list)
+    assert got == ans, print(f"dfs_list: {got=} != {ans=}")
+
+    got = dfs_list_iter(adj_list)
+    assert got == ans, print(f"dfs_list_iter: {got=} != {ans=}")
