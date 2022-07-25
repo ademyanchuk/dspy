3a4fb42205c60f729917301431ecca6532a99b0b
Alexey Demyanchuk <demyanchuk.alexey@mh-hannover.de>
2022-07-25 15:34:53 +0200

    Add bfs on adjacency list

--- dspy/graph.py
+++ dspy/graph.py
@@ -1,4 +1,4 @@
-from typing import List, Set
+from typing import Dict, List, Set, Tuple
 
 
 def dfs_visit_list(
@@ -45,6 +45,37 @@ def dfs_list_iter(adj_list: List[List[int]]) -> List[int]:
     return ans
 
 
+def bfs_list(
+    adj_list: List[List[int]], s: int
+) -> Tuple[Dict[int, int], Dict[int, int]]:
+    # keep parents dict, to recover shortest path if needed
+    level = {s: 0}
+    parent = {s: -1}
+    current = [s]
+    i = 1
+    while current:
+        next_level = []
+        for u in current:
+            for v in adj_list[u]:
+                if v not in level:
+                    level[v] = i
+                    parent[v] = u
+                    next_level.append(v)
+        current = next_level
+        i += 1
+    return level, parent
+
+
+def recover_path(parent, s, v):
+    if s not in parent or v not in parent:
+        return []
+    path = [v]
+    while v != s:
+        path.append(parent[v])
+        v = parent[v]
+    return list(reversed(path))
+
+
 if __name__ == "__main__":
     adj_list = [
         [5, 3],
@@ -54,9 +85,21 @@ if __name__ == "__main__":
         [],
         [2],
     ]
+    # test dfs
     ans = [2, 5, 1, 3, 0, 4]
     got = dfs_list(adj_list)
     assert got == ans, print(f"dfs_list: {got=} != {ans=}")
 
     got = dfs_list_iter(adj_list)
     assert got == ans, print(f"dfs_list_iter: {got=} != {ans=}")
+
+    # test bfs
+    s = 0
+    bfs_ans = {0: 0, 5: 1, 3: 1, 2: 2, 1: 2}
+    level, parent = bfs_list(adj_list, s)
+    assert level == bfs_ans, print(f"bfs_list: {level=} != {bfs_ans=}")
+    # and shortest path
+    v = 1
+    want_path = [0, 3, 1]
+    path = recover_path(parent, s, v)
+    assert path == want_path, print(f"recover path {s}->{v}: {path=} != {want_path=}")
